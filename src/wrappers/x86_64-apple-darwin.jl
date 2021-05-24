31f9e47929247140046f4ecc0bb9caa22a1abe2e
jlbuild <juliabuildbot@gmail.com>
2021-05-24 22:16:41 +0000

    libgraphicsmagic_jll build 1.3.36+1

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -2,13 +2,24 @@
 export gm, libGraphicsMagic, libGraphicsMagicPlusPlus, libGraphicsMagicWand
 
 using CompilerSupportLibraries_jll
+using libwebp_jll
+using libpng_jll
+using FreeType2_jll
+using Ghostscript_jll
+using Xorg_libXext_jll
+using Xorg_libSM_jll
+using Libtiff_jll
+using LittleCMS_jll
+using Bzip2_jll
+using XZ_jll
+using Libtool_jll
 JLLWrappers.@generate_wrapper_header("libgraphicsmagic")
 JLLWrappers.@declare_executable_product(gm)
 JLLWrappers.@declare_library_product(libGraphicsMagic, "@rpath/libGraphicsMagick.3.dylib")
 JLLWrappers.@declare_library_product(libGraphicsMagicPlusPlus, "@rpath/libGraphicsMagick++.12.dylib")
 JLLWrappers.@declare_library_product(libGraphicsMagicWand, "@rpath/libGraphicsMagickWand.2.dylib")
 function __init__()
-    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll)
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, libwebp_jll, libpng_jll, FreeType2_jll, Ghostscript_jll, Xorg_libXext_jll, Xorg_libSM_jll, Libtiff_jll, LittleCMS_jll, Bzip2_jll, XZ_jll, Libtool_jll)
     JLLWrappers.@init_executable_product(
         gm,
         "bin/gm",
